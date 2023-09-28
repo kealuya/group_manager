d620dfbff326174dc5d01be8556efc4be4c68ddc
任 <kealuya@126.com>
2023-09-28 16:06:13 +0800

    schoolSide，样式修改 追加 schoolSide 检索功能

--- backend/app/module/main/service/schoolList.ts
+++ backend/app/module/main/service/schoolList.ts
@@ -1,9 +1,9 @@
-import { AccessLevel, Inject, SingletonProto } from '@eggjs/tegg';
-import { EggLogger } from 'egg';
-import { EggMySQL } from 'egg-mysql';
+import { AccessLevel, Inject, SingletonProto } from "@eggjs/tegg";
+import { EggLogger } from "egg";
+import { EggMySQL } from "egg-mysql";
 
 @SingletonProto({
-    accessLevel: AccessLevel.PUBLIC,
+    accessLevel: AccessLevel.PUBLIC
 })
 
 export class SchoolList {
@@ -22,31 +22,32 @@ export class SchoolList {
                          b.bz
                         FROM
                          xt a
-                         LEFT JOIN school b ON a.school_code = b.school_code `
+                         INNER JOIN school b ON a.school_code = b.school_code `;
+        // left join 查询 会导致 出现空记录，在前端处理时，数组内有一条对象，但是schoolname、code都为空，需要改成inner join就不会有空数据 ren
         // INNER JOIN user c ON a.fzr1 = c.CODE  or a.fzr2 = c.CODE
         const sql2 = `WHERE 
                        a.fzr1 = :code  
-                        OR a.fzr2 = :code `
+                        OR a.fzr2 = :code `;
 
-        let sql = ''
+        let sql = "";
         if (isOwner) {
-            sql = sql1 + sql2
+            sql = sql1 + sql2;
 
         } else {
-            sql = sql1
+            sql = sql1;
 
         }
 
 
         const result = await this.mysql.query<Array<SchoolInfo>>(sql,
-            {
-                code: code,
-                isOwner: isOwner,
-            });
+          {
+              code: code,
+              isOwner: isOwner
+          });
         if (result.length > 0) {
-            return result
+            return result;
         }
-        return null
+        return null;
     }
 
 
--- src/components/SchoolList/schoolSide.vue
+++ src/components/SchoolList/schoolSide.vue
@@ -3,18 +3,19 @@
     <template #header>
       <div class="card-header">
         <div class="text">负责院校</div>
-        <el-input v-model="input1" class="w-50 m-2" placeholder="输入名称">
+        <el-input v-model="inputForSearch" class="w-50 m-2" placeholder="输入名称">
           <template #prefix>
             <el-icon class="el-input__icon">
               <search />
             </el-icon>
           </template>
         </el-input>
-        <el-checkbox class="checkbox" v-model="isOwner" label="本人负责" size="large" @change="getList"/>
+        <el-checkbox class="checkbox" v-model="isOwner" label="本人负责" size="large" @change="isOwnerChange" />
       </div>
     </template>
-    <el-scrollbar height="500px" class="scrollbar">
-      <div v-for="item in listData" :key="item.school_code" class="item" @click="chooseSchool(item.school_code,item.school_name)">
+    <el-scrollbar height="80%" class="scrollbar">
+      <div v-for="item in listData" :key="item.school_code" class="item"
+           @click="chooseSchool(item.school_code,item.school_name)">
         {{ item.school_name }}
       </div>
     </el-scrollbar>
@@ -30,36 +31,68 @@ import { toRaw } from "@vue/reactivity";
 import { useCommonStore } from "@/store/modules/common";
 import { storeToRefs } from "pinia";
 
-const input1 = ref("");
+const inputForSearch = ref("");
 const isOwner = ref(true);
-const listData =  ref([])
-const userStore = ref({})
-const emit = defineEmits(['getSchool'])
+const listData = ref([]);
+let listDataCopy = [];
+const userStore = ref({});
+const emit = defineEmits(["getSchool"]);
 const commonStore = useCommonStore();
 const { updateListValue } = storeToRefs(commonStore);
 
-const getList = async ()=>{
+const getList = async () => {
   const userStore = useUserStore();
-  const userInfo   = toRaw(userStore.userInfo) ;
-  let e = await getSchoolList1(userInfo.code,isOwner.value);
-  listData.value = e.data.data
-  commonStore.schoolListFirstCode = listData.value[0].school_code
-  commonStore.schoolListFirstName = listData.value[0].school_name
-  console.log('aaaaaaaaaaaaaaaaaa',commonStore.schoolListFirstName)
-}
+  const userInfo = toRaw(userStore.userInfo);
+  let e = await getSchoolList1(userInfo.code, isOwner.value);
+  listData.value = e.data.data;
+  listDataCopy = toRaw(listData.value);
+  commonStore.schoolListFirstCode = listData.value[0].school_code;
+  commonStore.schoolListFirstName = listData.value[0].school_name;
+};
 
-const chooseSchool = (code,name) => {
+const chooseSchool = (code, name) => {
 
-  commonStore.selectedSchoolCode = code
-  commonStore.selectedSchoolName = name
+  commonStore.selectedSchoolCode = code;
+  commonStore.selectedSchoolName = name;
   // emit('getSchool',name)
 };
-watch(() => updateListValue.value , ( ) => {
-  console.log('newValue, oldValue' )
-  getList()
-})
-onMounted( () => {
-   getList()
+watch(() => updateListValue.value, () => {
+  getList();
+});
+
+watch(() => inputForSearch.value, () => {
+
+  console.log(listDataCopy);
+  if (inputForSearch.value == "") {
+    listData.value = listDataCopy;
+    return;
+  }
+
+  const matchValueList = listDataCopy.find((value, index) => {
+    console.log(value);
+    if (value.school_name.indexOf(inputForSearch.value) >= 0) {
+      console.log(inputForSearch.value);
+      return true;
+    }
+  });
+
+  if (matchValueList == undefined) {
+    listData.value = [];
+    return;
+  }
+
+  listData.value = [];
+  listData.value.push(matchValueList);
+
+});
+
+const isOwnerChange = () => {
+  inputForSearch.value = "";
+  getList();
+};
+
+onMounted(() => {
+  getList();
 });
 
 </script>
